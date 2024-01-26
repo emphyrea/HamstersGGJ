32b13a9d3e5ab86d6fa43200df70dc93bd710fee
emphyrea <111723204+emphyrea@users.noreply.github.com>
2024-01-26 17:44:22 -0600

    aa

--- Assets/_ArianaAssets/Scripts/CinematicTracker.cs
+++ Assets/_ArianaAssets/Scripts/CinematicTracker.cs
@@ -1,18 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class CinematicTracker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] int timesPlayedCinematic = 0;
+    //[SerializeField] int timesPlayedCinematic = 0;
+    [SerializeField] PlayableDirector timeline;
 
-    // Update is called once per frame
-    void Update()
+    public static Action<bool> OnCinematicFinish;
+
+
+    private void Start()
     {
-        
+
+        if (PlayerPrefs.GetInt("Died") >= 1)
+        {
+            OnCinematicFinish?.Invoke(false);
+            timeline.Stop();
+            OnCinematicFinish?.Invoke(true);
+            Debug.Log("died, invoke");
+        }
+        else if (PlayerPrefs.GetInt("Died") <= 0)
+        {
+            timeline.time = 0;
+            timeline.Evaluate();
+            timeline.Play();
+            OnCinematicFinish?.Invoke(true);
+            //OnCinematicFinish?.Invoke(true);
+            if (timeline.time == timeline.playableAsset.duration)
+            {
+                {
+                    OnCinematicFinish?.Invoke(false);
+                    timeline.Stop();
+                }
+            }
+        }
     }
 }
+
+
+
